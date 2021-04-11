ef323f50984fc893328954e4e216584fa5ad05d9
Ezequiel <zapata.ed1989@gmail.com>
2021-04-11 13:12:10 -0300

    tests en useContext

---
+++ src/tests/components/09-useContext/LoginScreen.test.js
@@ -0,0 +1,45 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { LoginScreen } from '../../../components/09-useContext/LoginScreen';
+import { UserContext } from '../../../components/09-useContext/UserContext';
+
+describe('Pruenas en <LoginScreen />', () => {
+    
+    const setUser = jest.fn()
+    // wrapper
+    const wrapper = mount(
+        <UserContext.Provider value={{
+            setUser
+        }}>
+
+            <LoginScreen />
+
+        </UserContext.Provider>
+        
+        )
+
+    test('debe de mostrarse correctamente', () => {
+
+        //snapshot
+        expect( wrapper ).toMatchSnapshot()
+        
+    })
+
+    test('debe de ejecutar el setUser con el argumento esperado', () => {
+
+
+        wrapper.find('button').prop('onClick')()
+
+        expect( setUser ).toHaveBeenCalledWith({
+            id: 123,
+            name: 'eze'
+        })
+
+
+
+        
+    })
+    
+    
+    
+})
